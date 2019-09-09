5b19cdccdf3b2cb72df6f6495288702691af4afe
Anand Chitipothu <anandology@gmail.com>
2019-09-09 06:31:42 +0530

    Added terraform remote backend.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,14 @@
+terraform {
+  backend "remote" {
+    hostname = "app.terraform.io"
+    organization = "pipalacademy"
+
+    workspaces {
+      name = "ml-lab"
+    }
+  }
+}
+
 provider "aws" {
   access_key = var.aws_access_key
   secret_key = var.aws_secret_key
