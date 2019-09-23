4104865e7c7054209e62cff130dbf94f2cd0a0b5
Bargava <bargava@gmail.com>
2019-09-22 23:40:31 -0700

    Update post-install.sh

--- docker/post-install.sh
+++ docker/post-install.sh
@@ -1,6 +1,7 @@
 #! /bin/bash
 python -m spacy download en_core_web_lg
 apt-get update && apt-get -y install git graphviz
+pip install git+https://github.com/maciejkula/spotlight.git
 apt install wget unzip
 cd /tf/notebooks
 wget https://recommenderdata.s3.amazonaws.com/movielens/movielens.zip
