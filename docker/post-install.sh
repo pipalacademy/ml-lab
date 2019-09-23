072fcedec35d5b898f98141e6439376203faebf7
Bargava <bargava@gmail.com>
2019-09-22 23:35:24 -0700

    added link to data

--- docker/post-install.sh
+++ docker/post-install.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 python -m spacy download en_core_web_lg
 apt-get update && apt-get -y install git graphviz
+apt install wget unzip
+cd /tf/notebooks
+wget https://recommenderdata.s3.amazonaws.com/movielens/movielens.zip
+unzip movielens.zip
+rm movielens.zip
